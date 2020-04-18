12ed609bb025c8f5a6e41a15897a0a5a85e91af8
Yorgos Kakavas <yorgos.kakavas@exozet.com>
2020-04-18 19:32:09 +0300

    Added lazy loading bg image.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,19 @@ import Routes from '../../router/allRoutes';
 import './App.scss';
 
 class App extends Component {
+
+    handleLazy() {
+        const callback = () => {
+            const bgContainer = document.querySelector('html');
+            if (bgContainer) {
+                bgContainer.classList.add('lazy');
+            }
+        }
+        document.addEventListener('DOMContentLoaded', callback);
+    }
+
     render() {
+        this.handleLazy()
         return (
             <BrowserRouter>
                 {/* <Header/> */}
@@ -19,6 +31,10 @@ class App extends Component {
                 {/* <Footer/> */}
             </BrowserRouter>
         );
+    }
+
+    componentDidMount() {
+        this.handleLazy();
     }
 }
 
