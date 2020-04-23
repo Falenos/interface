4d82a2547a7ccbe5905c7b5d219a715f87a12859
Yorgos Kakavas <yorgos.kakavas@exozet.com>
2020-04-23 11:23:45 +0300

    Added muted.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import video from '../../assets/bg-video.mp4';
-// import video from '../../assets/file_example_MP4_1920_18MG.mp4';
 import placeholder from '../../assets/background.jpg';
 import Routes from '../../router/allRoutes';
 import './App.scss';
@@ -10,7 +9,7 @@ class App extends Component {
     render() {
         return (
             <Fragment>
-                <video className="video" autoPlay buffered="true" loop>
+                <video className="video" autoPlay buffered="true" muted loop poster={placeholder}>
                     <source src={video} type="video/mp4"/>
                 </video>
                 <BrowserRouter>
