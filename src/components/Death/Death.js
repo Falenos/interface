84b13366e866325de33f957d9069f72b3755aff3
Yorgos Kakavas <yorgos.kakavas@exozet.com>
2020-04-23 12:02:29 +0300

    Fixes.

--- src/components/Death/Death.js
+++ src/components/Death/Death.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { MAIN } from '../../router/namespaces';
 import './Death.scss'
 
 class DEATH extends Component {
@@ -8,8 +10,11 @@ class DEATH extends Component {
             <div className="death-container intro-container text-base container mx-auto animated-border border-solid border-2 border-teal-400 p-12">
                 <h2 className="mb-6 text-2xl text-teal-400 font-semibold">The end</h2>
                 <div>
-                    <p className="text-teal-400">Code by <a href="https://www.linkedin.com/in/yorgoskakavas">Yorgos Kakavas</a>. Source code repository <a href="https://github.com/Falenos/interface">here</a></p><br/>
+                    <p className="text-teal-400 mb-4">Code by <a href="https://www.linkedin.com/in/yorgoskakavas">Yorgos Kakavas</a>. Source code repository <a href="https://github.com/Falenos/interface">here</a></p><br/>
                 </div>
+                <Link to={{ pathname: MAIN }}>
+                    <button className="text-teal-400 border-solid border-2 border-teal-600 rounded px-12">Go Home</button>
+                </Link>
             </div>
         );
     }
