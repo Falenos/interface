0e437fb2a5d11bf973d6a80a60fb5f6604651cc7
Yorgos Kakavas <yorgos.kakavas@exozet.com>
2020-04-23 12:03:52 +0300

    Fixes.

--- src/components/Death/Death.js
+++ src/components/Death/Death.js
@@ -10,7 +10,7 @@ class DEATH extends Component {
             <div className="death-container intro-container text-base container mx-auto animated-border border-solid border-2 border-teal-400 p-12">
                 <h2 className="mb-6 text-2xl text-teal-400 font-semibold">The end</h2>
                 <div>
-                    <p className="text-teal-400 mb-4">Code by <a href="https://www.linkedin.com/in/yorgoskakavas">Yorgos Kakavas</a>. Source code repository <a href="https://github.com/Falenos/interface">here</a></p><br/>
+                    <p className="text-teal-400 mb-4">Code by <a target="_blank" href="https://www.linkedin.com/in/yorgoskakavas">Yorgos Kakavas</a>. Source code repository <a target="_blank" href="https://github.com/Falenos/interface">here</a></p><br/>
                 </div>
                 <Link to={{ pathname: MAIN }}>
                     <button className="text-teal-400 border-solid border-2 border-teal-600 rounded px-12">Go Home</button>
